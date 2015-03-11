ea05f44b5322aaed05455e07885af405cdfcfe01
jjprada <jjprada@gmail.com>
2015-03-11 19:45:26 +0100

    First Version

---
+++ app/src/main/java/com/jjprada/mycontacts/Contact.java
@@ -0,0 +1,17 @@
+package com.jjprada.mycontacts;
+
+/**
+ * Created by Dr4ckO on 11/03/2015.
+ */
+public class Contact {
+
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
---
+++ app/src/main/java/com/jjprada/mycontacts/ContactListActivity.java
@@ -0,0 +1,75 @@
+package com.jjprada.mycontacts;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class ContactListActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+        Contact contact1 = new Contact();
+        contact1.setName("José Prada");
+        contacts.add(contact1);
+
+
+        ListView listView = (ListView)findViewById(R.id.contact_list_view);
+        listView.setAdapter(new ContactAdapter(contacts));
+
+
+    }
+
+    private class ContactAdapter extends ArrayAdapter<Contact>{
+        ContactAdapter(ArrayList<Contact> contacts){
+            super(ContactListActivity.this, R.layout.contact_list_row, R.id.contact_row_name, contacts);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+
+            Contact contact = getItem(position);        // Obtenemos el contacto
+
+            TextView textViewName = (TextView)convertView.findViewById(R.id.contact_row_name);      // Buscamos el elemento al cual queremos modificarle el nombre
+            textViewName.setText(contact.getName());            // Hacemos que el texto de la vista muestre el nombre del contacto
+
+            return convertView;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_contact_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
